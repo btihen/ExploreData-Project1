32b332871b3122149499cb586fdb2aa3ca8d9dc1
Bill Tihen <btihen@gmail.com>
2014-11-08 23:41:02 +0100

    Changed the code to be consistent and easier to write plot2

--- plot1.R
+++ plot1.R
@@ -1,26 +1,28 @@
 # read the data file
 data<-read.csv("household_power_consumption.txt",sep=";")
 
-# convert factors to usable data
-data$Date <- as.Date(data$Date, format="%d/%m/%Y")
-data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+# convert date to usable data
+data$date <- as.Date(data$Date, format="%d/%m/%Y")
 
-# create the plot data set between the proper dates
+# select the data in the dates of interest
 DATE1 = as.Date("2007-02-01")
 DATE2 = as.Date("2007-02-02")
-plot1_df <- data[data$Date >= DATE1 & data$Date <= DATE2,]
+plot_df <- data[data$date >= DATE1 & data$date <= DATE2,]
 
-# Let me see the graph
-hist(plot1_df$Global_active_power,col="red",
+# make the graphing data usable
+plot_df$Global_active_power <- as.numeric(as.character(plot_df$Global_active_power))
+
+# create I can see the graph
+hist(plot_df$Global_active_power,col="red",
      main="Global Active Power",
-     xlab="Global Active Power (kilowats)")
+     xlab="Global Active Power (kilowatts)")
 
 # create the plot as a png
 png(filename = "plot1.png", width = 480, height = 480)
 #    units = "px", bg = "transparent")
 #    units = "px", pointsize = 12, bg = "white", res = NA,
 #    restoreConsole = TRUE)
-hist(plot1_df$Global_active_power,col="red",
+hist(plot_df$Global_active_power,col="red",
      main="Global Active Power",
-     xlab="Global Active Power (kilowats)")
+     xlab="Global Active Power (kilowatts)")
 dev.off()
